d2ec6cbc55a4279fee40b4d36b7aaeba2b0e2eea
Stan Etra <stanetra@gmail.com>
2026-06-18 16:25:43 -0400

    Finished section 2.2

--- Work/02_Working_With_Data.py
+++ Work/02_Working_With_Data.py
@@ -27,3 +27,49 @@ with open('Data/prices.csv', 'rt') as f:
             prices[row[0]] = float(row[1])
         except (ValueError, IndexError):
             print(line)
+
+# Exercise 2.6
+# List of dicts
+import csv
+def read_prices(filename):
+    prices = {}
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            try:
+                name = row[0]
+                price = float(row[1])
+                prices[name] = price
+            except (ValueError, IndexError):
+                pass
+    return prices
+
+prices = read_prices('Data/prices.csv')
+print(prices)
+
+# Excercise 2.7
+def read_portfolio_with_calcs(filename):
+    portfolio = []
+    prices = read_prices('Data/prices.csv')
+    port_value = 0.0
+    port_pnl = 0.0
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        header = next(rows)
+        for row in rows:
+            try:
+                name = row[0]
+                shares = int(row[1])
+                price = float(row[2])
+                cost = shares * price
+                value = shares * prices[name]
+                holding = {'name': name,'shares': shares,'price':price, 'cost':cost, 'value':value}
+                portfolio.append(holding)
+                port_value += value
+                port_pnl += value - cost
+            except (ValueError, IndexError):
+                pass   
+    return portfolio, port_value, port_pnl
+
+portfolio, port_value, port_pnl = read_portfolio_with_calcs('Data/portfolio.csv')
+print(f"Portfolio Value: {port_value} Portfolio PnL: {port_pnl}")
